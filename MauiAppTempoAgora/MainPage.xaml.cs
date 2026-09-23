a32cb10ebd5c36ef0202a153d906a9c89968055c
Elias <eliasmarra0@gmail.com>
2026-09-23 15:07:53 -0300

    Melhorias adicionais

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -10,7 +10,7 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-       private async void Button_Clicked(object sender, EventArgs e)
+     private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -40,6 +40,14 @@ namespace MauiAppTempoAgora
 
                         lbl_res.Text = dados_previsao;
 
+                        string mapa = $"<html><body style='margin:0'>" +
+                                      $"<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" src=\"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}\">" +
+                                      $"</iframe></body></html>";
+
+                        wv_mapa.Source = new HtmlWebViewSource { Html = mapa };
+
                     }
                     else
                     {
@@ -55,6 +63,67 @@ namespace MauiAppTempoAgora
             } catch (Exception ex)
             {
                 await DisplayAlertAsync("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                        GeolocationAccuracy.Medium,
+                        TimeSpan.FromSeconds(10)
+                    );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if(local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude} \n";
+
+                    lbl_coords.Text = local_disp;
+
+                    GetCidade(local.Latitude, local.Longitude);
+
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlertAsync("Erro: Dispositivo não suporta.", fnsEx.Message, "OK");
+            }  
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlertAsync("Erro: Localização Desabilitada.", fneEx.Message, "OK");
+            }
+            catch (PermissionException peEx)
+            {
+                await DisplayAlertAsync("Erro: Permissão da Localização negada.", peEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Erro: Obtenção de cidade falhou", ex.Message, "OK");
             }
         }
     }
